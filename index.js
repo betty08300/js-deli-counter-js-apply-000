c5011c210b4d58d82348830fee22d994c3213d59
favorable-actor-6400 <wreny641@gmail.com>
2019-04-18 18:32:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function takeANumber(currentLine, name){
-  for (var i = 0; i < currentLine.length; i++) {
-    var currentPerson = currentLine[i];
-    if (name === currentPerson) {
-      return `Welcome, ${name}. You are number ${i + 1} in line.`;
-    }
-  }
+  currentLine.push(name);
+  return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
+
 }
 
 function nowServing(currentLine){
