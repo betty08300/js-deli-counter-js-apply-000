9ff9341910b921c2a9d130ed735fbd6f7c423daf
favorable-actor-6400 <wreny641@gmail.com>
2019-04-18 18:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function nowServing(currentLine){
 
 function currentLine(line) {
   if (line.length === 0) {
-    return = 'The line is currently empty.';
+    return 'The line is currently empty.';
   }
   var result = 'The line is currently: '
   for (var i = 0; i < line.length; i++) {
