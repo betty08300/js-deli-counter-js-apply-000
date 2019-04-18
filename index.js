6aa5d8af447f090ba5c0066118c19d2742138d97
favorable-actor-6400 <wreny641@gmail.com>
2019-04-18 18:19:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function takeANumber(currentLine, name){
   return `Welcome, ${name}. You are number ${currentLine} in line.`
 }
+
+function nowServing(currentLine){
+  for (var i = 0; i < currentLine.length; i++) {
+    var current = currentLine[i];
+    return current;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
