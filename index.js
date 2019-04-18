eb2698a6a2943cd442bae1cdcc1ae17ce02afe72
favorable-actor-6400 <wreny641@gmail.com>
2019-04-18 18:42:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function currentLine(line) {
   if (line.length === 0) {
     return 'The line is currently empty.';
   }
-  var result = 'The line is currently: '
+  var result = 'The line is currently:'
   for (var i = 0; i < line.length; i++) {
     var name = line[i];
-    result += `${i+1}. ${name},`
+    result += ` ${i+1}. ${name}`
   }
   return result;
 }
